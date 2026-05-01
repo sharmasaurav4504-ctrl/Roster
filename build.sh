405ce2fa3e763bce74a660c062ff8785d188c5f8
Saurav Sharma <sharmasaurav4504@gmail.com>
2026-05-01 16:54:02 +0530

    Initial commit: Add project files and ignore SQLite DB

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+
+# Create superuser safely
+python manage.py shell << END
+from django.contrib.auth.models import User
+
+username = "wfm_admin"
+password = "admin123"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, password=password)
+    print("Superuser created")
+else:
+    print("Superuser already exists")
+END
